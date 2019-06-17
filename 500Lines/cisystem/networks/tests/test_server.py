2a54c2d703497dd81902fbc20b988e793106e0e5
Kostas Makedos <kostas.makedos@gmail.com>
2019-06-17 23:44:41 +0300

    Trying to make unit tests work

--- 500Lines/cisystem/networks/tests/test_server.py
+++ 500Lines/cisystem/networks/tests/test_server.py
@@ -1,22 +1,22 @@
 from unittest import TestCase
 import socket
-import utilities
+from networks import utilities
 import multiprocessing
 
 
 
 class TestConnections(TestCase):
     def setUp(self):
-        self.Server = utilities.Server()
-        self.Client = utilities.Client()
+        pass
 
     def test_run(self):
-        self.serverProcess = multiprocessing.Process(target = self.Server.run)
-        self.clientProcess = multiprocessing.Process(target = self.Client.send_message, args=("123324",))
+        self.serverProcess = multiprocessing.Process(name = utilities.Server.run)
+        self.clientProcess = multiprocessing.Process(name = utilities.Client)
         self.serverProcess.start()
         self.clientProcess.start()
         self.serverProcess.join()
         self.clientProcess.join()
+
 
 
     def test_stop(self):
--- 500Lines/cisystem/networks/utilities.py
+++ 500Lines/cisystem/networks/utilities.py
@@ -5,58 +5,39 @@
 #  Copyright (c) 2019
 #  All rights reserved.
 
+import socketserver
 import socket
 
 
-class Server:
+class RequestHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler must be instantiated once per connection and must
+    override the handle() method to implement communication to the client
+    """
 
-    def __init__(self, hostIP="0.0.0.0", port=9000):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self._hostIP = hostIP
-        self._port = port
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+
+
+class Server():
+
+    def __init__(self, host="0.0.0.0", port=9000):
+        self.server = socketserver.TCPServer((host, port), RequestHandler)
+        self.server.allow_reuse_address = True
 
     def run(self):
-        self._socket.bind((self._hostIP, self._port))
-        self._socket.listen(1)
-        while True:
-            c,addr = self._socket.accept()
-            print("Got connection from ",  addr)
-            while True:
-                mesg = self._recv_all(c, 1024)
-                if mesg:
-                    print(mesg)
-                    exit()
-                else:
-                    break
-
-
-    def _recv_all(self, client_socket, length):
-        data = ''
-        while len(data) < length:
-            more = client_socket.recv(length - len(data))
-            if not more:
-                return data
-            data += more.decode()
-        return data
-
-
-
-    def stop(self):
-        self._socket.shutdown(socket.SHUT_RDWR)
-        self._socket.close()
-
-
+        with self.server:
+            self.server.serve_forever()
 
 class Client:
     def __init__(self, hostIP="0.0.0.0", port=9000):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._hostIP = hostIP
         self._port = port
-
+        
     def send_message(self, message):
         self._socket.connect(((self._hostIP, self._port)))
         self._socket.send(message.encode())
-
-
-
